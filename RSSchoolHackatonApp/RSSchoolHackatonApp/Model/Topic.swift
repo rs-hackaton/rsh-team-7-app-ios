48adcad684d735b4151824f1face19c17a19c9c7
Arseniy Strakh <ArseniiStrakh@gmail.com>
2020-08-01 13:51:51 +0300

    feat: add TableViewController

---
+++ RSSchoolHackatonApp/RSSchoolHackatonApp/Model/Topic.swift
@@ -0,0 +1,15 @@
+//
+//  Topic.swift
+//  RSSchoolHackatonApp
+//
+//  Created by Arseniy Strakh on 01.08.2020.
+//  Copyright © 2020 Alex K. All rights reserved.
+//
+
+import Foundation
+
+struct Topic {
+  var name: String
+  var date: Date
+  
+}
