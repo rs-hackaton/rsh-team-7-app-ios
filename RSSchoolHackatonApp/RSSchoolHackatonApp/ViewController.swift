5231b6f48fe0b886a9ee97be2ab8c796ea6dc2d5
alexeykomov <alexeykofficial@gmail.com>
2020-08-01 13:05:17 +0300

    init: project structure

---
+++ RSSchoolHackatonApp/RSSchoolHackatonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSchoolHackatonApp
+//
+//  Created by Alex K on 8/1/20.
+//  Copyright © 2020 Alex K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
