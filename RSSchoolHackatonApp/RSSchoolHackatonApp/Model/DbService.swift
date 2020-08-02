9611135d6e7ebd1e470a4e87096ed5827db9f12f
alexeykomov <alexeykofficial@gmail.com>
2020-08-02 18:34:29 +0300

    feat: fetching of topics and subscribing to updates

--- RSSchoolHackatonApp/RSSchoolHackatonApp/Model/DbService.swift
+++ RSSchoolHackatonApp/RSSchoolHackatonApp/Model/DbService.swift
@@ -28,13 +28,14 @@ protocol Observable {
 protocol RoomServiceType: Observable {
     func createRoom(with title: String, completion:  @escaping (Room?, ServiceError?) -> Void)
     func fetchRoomInfo(completion: @escaping (Room?, ServiceError?) -> Void)
-    func fetch(completion: ([Topic]) -> Void)
+    func fetch(completion: @escaping ([Topic]) -> Void)
+    func subscribeForUpdates(onAdd: @escaping (Topic)-> Void, onRemove: @escaping (Topic) -> Void) -> () -> Void
     func update(topic: Topic)
     func add(topic: Topic)
     func remove(topic: Topic)
 }
 
-class RoomService: RoomServiceType {
+class DbService: RoomServiceType {
 
     // MARK: -
 
@@ -123,15 +124,84 @@ class RoomService: RoomServiceType {
         }
     }
 
-    func fetch(completion: ([Topic]) -> Void) {
-        completion([])
+    func fetch(completion: @escaping ([Topic]) -> Void) {
+        guard let userId = userId else {
+            return
+        }
+        guard let roomId = roomId, !roomId.isEmpty else {
+            return
+        }
+        Storage.getInstance().ref?.child("topics").queryOrdered(byChild: "roomId").queryEqual(toValue: roomId).observeSingleEvent(of: .value, with: { (snapshot) in
+            print("Snapshot value: \(snapshot.value)")
+            guard let value = snapshot.value as? Dictionary<String, Dictionary<String, Any>> else {
+                return
+            }
+            let topics:[Topic] = value.map {key, topicAsDict in
+                let title = topicAsDict["title"] as? String ?? ""
+                let time = Date(timeIntervalSince1970: TimeInterval(Double(topicAsDict["time"] as? String ?? "") ?? 0))
+                let active = Bool(topicAsDict["active"] as? String ?? "false") ?? false
+                let order = Int(topicAsDict["active"] as? String ?? "0") ?? 0
+                let topic = Topic(id: key, title: title, time: time, roomId: roomId, active: active, order: order, userId: userId)
+                return topic
+            }
+            completion(topics)
+        }) {
+            print($0.localizedDescription)
+        }
+    }
+    
+    func subscribeForUpdates(onAdd: @escaping (Topic) -> Void, onRemove: @escaping (Topic) -> Void) -> () -> Void {
+        guard let ref = Storage.getInstance().ref else { return {} }
+        let addObserverHandle = ref.child("topics").queryOrdered(byChild: "roomId").queryEqual(toValue: roomId).observe(.childAdded, with: { (snapshot) in
+            guard let dict = snapshot.value as? Dictionary<String, Any> else {
+                return
+            }
+            let topic = Topic.fromDict(dict, id: snapshot.key)
+            onAdd(topic)
+            print("Add listener: \(snapshot.value)")
+            
+        })
+        let removeObserverHandle = ref.child("topics").queryOrdered(byChild: "roomId").queryEqual(toValue: roomId).observe(.childRemoved, with: { (snapshot) in
+            guard let dict = snapshot.value as? Dictionary<String, Any> else {
+                return
+            }
+            let topic = Topic.fromDict(dict, id: snapshot.key)
+            onRemove(topic)
+            print("Remove listener: \(snapshot.value)")
+        })
+        return {
+            ref.removeObserver(withHandle: addObserverHandle)
+            ref.removeObserver(withHandle: removeObserverHandle)
+        }
     }
 
     func update(topic: Topic) {
         print(#function)
     }
     func add(topic: Topic) {
-        print(#function)
+        guard let userId = userId else {
+            return
+        }
+        guard let roomId = roomId else {
+            return
+        }
+        guard let ref = Storage.getInstance().ref else {
+            return
+        }
+        ref.child("topics").childByAutoId().setValue([
+            "active": false,
+            "order": 0,
+            "roomId": roomId,
+            "time": String(Date().timeIntervalSince1970),
+            "title": topic.title,
+            "userId": userId,
+            ], withCompletionBlock: { (error, ref) in
+                guard error != nil else {
+                    return
+                }
+                ref.observe(.value) { [weak self] (snapshot) in
+                }
+        })
     }
     func remove(topic: Topic) {
         print(#function)
--- RSSchoolHackatonApp/RSSchoolHackatonApp/Model/Topic.swift
+++ RSSchoolHackatonApp/RSSchoolHackatonApp/Model/Topic.swift
@@ -9,10 +9,22 @@
 import Foundation
 
 struct Topic {
+    let id: String
     var title = ""
     var time = Date()
     var roomId = ""
     var active: Bool = false
     var order: Int = 0
     var userId: String = ""
+    
+    static func fromDict(_ topicAsDict: Dictionary<String, Any>, id: String) -> Topic {
+        let title = topicAsDict["title"] as? String ?? ""
+        let time = Date(timeIntervalSince1970: TimeInterval(Double(topicAsDict["time"] as? String ?? "") ?? 0))
+        let active = Bool(topicAsDict["active"] as? String ?? "false") ?? false
+        let order = Int(topicAsDict["active"] as? String ?? "0") ?? 0
+        let roomId = topicAsDict["roomId"] as? String ?? ""
+        let userId = topicAsDict["userId"] as? String ?? ""
+        let topic = Topic(id: id, title: title, time: time, roomId: roomId, active: active, order: order, userId: userId)
+        return topic
+    }
 }
